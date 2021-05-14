7df328aa928d4cf64fb4c53351e015478d5f88b6
namuHlaeR <77580956+namuHlaeR@users.noreply.github.com>
2021-05-14 16:46:37 +0200

    Firs real commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoBtn = document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
+
+// Event listeners
+todoBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+// Functions
+
+function addTodo(event){
+    // Prevent from refresh
+    event.preventDefault();
+    // Todo DIV 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create an LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check btn
+    const completedBtn = document.createElement('button');
+    completedBtn.innerHTML = '<i class="fas fa-check"></i>';
+    completedBtn.classList.add('completed-btn');
+    todoDiv.appendChild(completedBtn);
+    // Delete btn
+    const trash = document.createElement('button');
+    trash.innerHTML = '<i class="fas fa-trash"></i>';
+    trash.classList.add('trash-btn');
+    todoDiv.appendChild(trash);
+    // Apend to list
+    todoList.appendChild(todoDiv);
+    // Clear input value
+    todoInput.value = '';
+    // Testing
+    console.log('Hey')
+}
+// Delete & Check btn's
+function deleteCheck(e){
+    const item = e.target;
+    // Delete todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitioned', function(){
+            todo.remove();
+        });
+    }
+    // Check item
+    if(item.classList[0] === 'completed-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+};
